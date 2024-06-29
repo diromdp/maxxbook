0b7ced401bafe45b67c30a1882e201b1cde7ff14
diromp <dirom@catalyzecommunications.com>
2024-06-30 00:13:18 +0700

    fixing bug

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -1,4 +1,4 @@
 // export const urlAPI = "http://127.0.0.1:8000/";
-export const urlAPI = "https://api.urbannesia.com/";
-//export const BaseUrl = "https://fe.urbannesia.com";
-export const BaseUrl = "http://localhost:3000";
+export const urlAPI = "https://api.maxibook.co/";
+export const BaseUrl = "https://maxibook.co";
+//export const BaseUrl = "http://localhost:3000";
