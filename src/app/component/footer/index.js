7cfab45c51293de680c383dd3fc5cbfb067743c5
diromp <dirom@catalyzecommunications.com>
2024-06-29 22:13:29 +0700

    update progress

--- src/app/component/footer/index.js
+++ src/app/component/footer/index.js
@@ -37,7 +37,7 @@ const Footer = () => {
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="/" className="hover:underline">Maxibook</a>. All Rights Reserved.
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} <a href="/" className="hover:underline">Maxibook</a>. All Rights Reserved.
                     </span>
                     <div className="flex mt-4 sm:justify-center sm:mt-0">
                         {/* <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white h-[16px] w-[16px]">
