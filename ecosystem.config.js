e8b4df74c222514c298bf7df20085a5a4c418462
diromp <dirom@catalyzecommunications.com>
2024-08-14 18:44:46 +0700

    update progress

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,9 @@ module.exports = {
         cwd: "/var/www/maxxbook",
         autorestart: true,
         watch: false,
-        instances: '4',
+        instances: '1',
         exec_mode: 'cluster',
-        max_memory_restart: '1G',
+        max_memory_restart: '2G',
         env: {
             NODE_ENV: "production",
             PORT: 3000
